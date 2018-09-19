bd5ef45c51476b3b8ccf0ddd7fa28dbe071e3b92
Makoto.Kaneko <mako_080809-xue@yahoo.co.jp>
2018-09-19 11:49:48 +0900

    Initial Commit

---
+++ test1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by Makoto Kaneko on 2018/09/19.
+//  Copyright © 2018年 Makoto Kaneko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
