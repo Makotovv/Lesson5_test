5748fe936e154a787492d648b2a96a44c4268252
Makoto.Kaneko <mako_080809-xue@yahoo.co.jp>
2018-09-19 23:47:32 +0900

    途中経過。変更したか中身を整理しただけ。RUNはOK

--- test1/ViewController.swift
+++ test1/ViewController.swift
@@ -11,23 +11,36 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var imageview: UIImageView!
     
+    
+    
+    
+    
     @IBAction func behind(_ sender: Any) {
         
 //buttonの有効、無効化
 //×       behind.enabled = false
 //×       behind.enabled = true
-// if  スライド動いている時は無効にさせる
+// if  スライド動いている時は無効にさせる・再生/停止ボタンで停止の表示中はボタン無効でもある
         
         dispImageNO += 1
         displayImage()
     }
     @IBAction func Forward(_ sender: Any) {
+// if  スライド動いている時は無効にさせる・再生/停止ボタンで停止の表示中はボタン無効でもある
+        
+        
         dispImageNO -= 1
         displayImage()
+        
+        
     }
     @IBAction func auto(_ sender: Any) {
-        //タイマー設定
+//タイマー設定
     timer = Timer.scheduledTimer (timeInterval: 2.0, target: self, selector : #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+//auto時のラベル変更
+   
+        
+        
     }
 
 //一定の間隔で処理を行うためのタイマー
@@ -35,6 +48,7 @@ class ViewController: UIViewController {
     
 //表示している画像の番号
     var dispImageNO = 0
+    
     
 //表示している画像の番号を元に画像を表示する
     func displayImage(){
@@ -48,33 +62,37 @@ class ViewController: UIViewController {
             dispImageNO = 0
         }
 //表示している画像の番号から名前を取り出し
-        let name=imageNameArray[dispImageNO]
 //画像の読み込み
+//Imageviewに読み込んだ画像をセット
+        let name=imageNameArray[dispImageNO]
         let image = UIImage(named: name)
-//Image viewに読み込んだ画像をセット
         imageview.image = image
         }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//①バンドルした画像ファイルを読み込み。②UIImageViewに画像をくくりつけ
-       let image = UIImage(named : "ANA.jpg")
-      imageview.image = image
         
- //タイマー設定
-   //     timer = Timer.scheduledTimer (timeInterval: 2.0, target: self, selector : #selector(updateTimer(_:)), userInfo: nil, repeats: true)
-    }
-    //NSTimaerによって一定の間隔で呼び出される関数
+//バンドルした画像ファイルを読み込み。 コメント0919　→ 初期の画像
+//UIImageViewに画像をセット        コメント0919 → 初期の画像
+         let image = UIImage(named : "ANA.jpg")
+         imageview.image = image
+//タイマー設定  → ボタンへ移動
+//     timer = Timer.scheduledTimer (timeInterval: 2.0, target: self, selector : #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+        }
+    
+    
+//imaerによって一定の間隔で呼び出される関数
     @objc func updateTimer(_ timer: Timer) {
         // 関数が呼ばれていることを確認
-        //print("onTimer")
+        print("onTimer")
 //表示している画像の番号を1増やす
         dispImageNO += 1
-        
 //表示している画像の番を元に画像を表示
         displayImage()
-        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -82,13 +100,14 @@ class ViewController: UIViewController {
     }
 }
 
-//プロジェクトを新規作成し、 SlideshowApp というプロジェクト名をつけてください
+
 //1つ目の画面ではスライドショー, 2つ目の画面では拡大画像を表示します   >>>> セグエ紐付けまで実施
 //自動送りの間は、進むボタンと戻るボタンはタップ不可にしてください
 //再生ボタンをタップすると停止ボタンになり、停止ボタンをタップすると再生ボタンにしてください
 //停止ボタンをタップすると自動送りが止まり、進むボタンと戻るボタンをタップ可能にしてください
 //画像をタップすると画面を遷移させ、拡大画像と戻るボタンを表示させてください
 //戻るボタンがタップされると、スライドショー画面に戻り、同じ画像を表示してください
+//プロジェクトを新規作成し、 SlideshowApp というプロジェクト名をつけてください
 //Auto Layoutを使用して、iPhone SE, iPhone 8, iPhone 8 Plus, iPhone Xの各画面サイズでレイアウトが崩れないようにしてください
 ////画面を2つ作成してください
 ////スライドショー画面には、画像と3つのボタン（進む、戻る、再生/停止）を配置してください
--- test1/secondViewController.swift
+++ test1/secondViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class secondViewController: UIViewController {
+    
+    
+
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
